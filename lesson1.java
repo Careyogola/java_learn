733e023d8afc1094b89ababfc5f45075462882c8
Carey Ogola <careyogola@Carey-Ogola-M1.local>
2025-10-26 18:04:50 +0300

    comments

--- lesson1.java
+++ lesson1.java
@@ -5,9 +5,9 @@ class lesson1 {
     // declaring and initializing variables outside the main 
     //use static keyword 
 
-     //static int age = 23;
-    // static int number1 = 13;
-     //static int number2 = 17;
+     static int age = 23;
+    static int number1 = 13;
+     static int number2 = 17;
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
@@ -17,13 +17,13 @@ class lesson1 {
         System.out.println(name);
 
         scanner.close();
-        //int age = 23;
+        int age = 23;
         //equal to operator
-        //System.out.println(number1 == number2);
+        System.out.println(number1 == number2);
         //not equal to operator
-       // System.out.println(number1 != number2);
-        //greater than or equal to
-        //System.out.println(number1 >= number2);
-        //System.out.println(number1 == number2);
+       System.out.println(number1 != number2);
+       // greater than or equal to
+        System.out.println(number1 >= number2);
+        System.out.println(number1 == number2);
     }
 }
